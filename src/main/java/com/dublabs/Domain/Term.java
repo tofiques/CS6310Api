58cfe36b3f778021fcaea9485aafef583f779824
jineshk77 <jinesh.kumar@gmail.com>
2017-12-04 21:52:14 -0500

    add term object

--- src/main/java/com/dublabs/Domain/Term.java
+++ src/main/java/com/dublabs/Domain/Term.java
@@ -1,8 +1,15 @@
 package com.dublabs.Domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by tofiques on 12/3/17.
  */
+@Entity
+@Table(name = "term", schema = "csassignment")
 public class Term {
      private  Integer termId;
 
@@ -10,6 +17,12 @@ public class Term {
         this.termId = termId;
     }
 
+    public Term() {
+		
+	}
+
+	@Id
+    @Column(name = "term_id")
     public Integer getTermId() {
         return termId;
     }
